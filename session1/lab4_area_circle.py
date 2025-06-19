bbdbec0aa5dd6d8605a7294c68e02b41fdceba83
Moatasem El Sayed <eng.moatasem.8@gmail.com>
2025-06-19 17:38:24 +0200

    test

---
+++ session1/lab4_area_circle.py
@@ -0,0 +1,20 @@
+"""Write a Python program which accepts the radius of a circle and compute the area."""
+
+import math
+
+
+def area(number1):
+    """
+    Write a Python program which accepts the radius of a circle from the argument
+    and compute the area. Don't forget to return the result at the end.
+    """
+    res = math.pi * (number1**2)
+    print(f"Area of the circle with radius {number1} is: {res}")
+    return res
+
+
+if __name__ == "__main__":
+    assert area(2) == 12.566370614359172, "Test case failed"
+    assert area(1) == 3.141592653589793, "Test case failed"
+    assert area(0) == 0.0, "Test case failed"
+    assert area(5) == 78.53981633974483, "Test case failed"
