b3663e109297f91795f53fa9d7ca01ca0003272c
SeragKhaled <seragkhaled120@gmail.com>
2025-06-25 17:44:39 +0300

    Solutions for session1_SeragKhaled

--- session1/lab5_accumulator.py
+++ session1/lab5_accumulator.py
@@ -3,7 +3,11 @@
 
 def compute(number):
     """write your soulution Here Sum 1 … n — Compute and display the running total."""
-
+    total = 0
+    for i in range(number):
+        total += i+1
+    print(total)
+    return total
 
 if __name__ == "__main__":
     assert compute(5) == 15, "Test case failed"
