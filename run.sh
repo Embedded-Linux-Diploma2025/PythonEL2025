d548cf6748e58afd6457fbe97c88796e670eade7
Abdelrahman Fekry <abdelrahmanfekry375@gmail.com>
2025-06-28 14:59:33 +0300

    sess 1 only

--- run.sh
+++ run.sh
@@ -15,17 +15,17 @@ for file in *.py; do
     echo "[🟩] $file ran successfully"
 done
 #----------------- Session 2 -----------------#
-cd ../session2 || exit 1
-for file in *.py; do
-    pylint "$file"
-    if [ $? -ne 0 ]; then
-        echo "[🟥] Error running pylint for $file"
-        exit 1
-    fi
-    python "$file"
-    if [ $? -ne 0 ]; then
-        echo "[🟥] Error running $file"
-        exit 1
-    fi
-    echo "[🟩] $file ran successfully"
-done
+#cd ../session2 || exit 1
+#for file in *.py; do
+#    pylint "$file"
+#    if [ $? -ne 0 ]; then
+#        echo "[🟥] Error running pylint for $file"
+#        exit 1
+#    fi
+#    python "$file"
+#    if [ $? -ne 0 ]; then
+#        echo "[🟥] Error running $file"
+#        exit 1
+#    fi
+#    echo "[🟩] $file ran successfully"
+#done
