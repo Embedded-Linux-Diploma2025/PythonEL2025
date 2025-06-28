bdf128117c39232996d23523ed1969e4f553391d
Abdulrhman <abdelrahman.a.elshenawy@gmail.com>
2025-06-29 00:58:05 +0300

    Add location lap

--- session2/lab1_get_your_location.py
+++ session2/lab1_get_your_location.py
@@ -2,11 +2,18 @@
 
 import requests
 
-
 def get_info_location():
     """Write your solution here. Don't forget to return the result at the end."""
-
-
+    response_content = requests.get('http://ip-api.com/json/24.48.0.1',timeout=5).json()
+    response_dic = {
+        "ip":response_content["query"],
+        "city":response_content["city"],
+        "region":response_content["region"],
+        "country":response_content["country"],
+        "loc":"1",
+        "org":response_content["org"]
+    }
+    return response_dic
 if __name__ == "__main__":
     location_info = get_info_location()
     assert "ip" in location_info, "Test case failed"
