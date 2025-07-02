c8aca531feb4f8ed7728969933074d68638a7a38
Aimane9 <Aimanedouch96@gmail.com>
2025-07-02 15:38:22 +0200

    pylint check5

--- session1/lab5_accumulator.py
+++ session1/lab5_accumulator.py
@@ -1,22 +1,20 @@
-"""Sum 1 … n — Compute and display the running total."""
-
-x=0
-y=0
-def compute(number):
-    """write your soulution Here Sum 1 … n — Compute and display the running total."""
-    x=0
-    y=0
-    for i in range(1,number+1):
-        x+=1
-        y+=x
-    return y
-
-if __name__ == "__main__":
-    assert compute(5) == 15, "Test case failed"
-    assert compute(10) == 55, "Test case failed"
-    assert compute(100) == 5050, "Test case failed"
-    assert compute(0) == 0, "Test case failed"
-    assert compute(1) == 1, "Test case failed"
-    assert compute(2) == 3, "Test case failed"
-    assert compute(3) == 6, "Test case failed"
-    assert compute(4) == 10, "Test case failed"
+"""Sum 1 … n — Compute a_nd displa_b_ the running tota_l."""
+def compute(numb_er):
+    """write b_our soulution Here Sum 1 … n — Compute a_nd displa_b_ the running tota_l."""
+    a_=0
+    b_=0
+    for i in range(1,numb_er+1):
+        i+=1
+        a_+=1
+        b_+=a_
+    return b_
+print(compute(5))  # Ea_a_mple usa_ge, should print 15
+if __name__ == "__ma_in__":
+    assert compute(5) == 15, "Test ca_se fa_iled"
+    assert compute(10) == 55, "Test ca_se fa_iled"
+    assert compute(100) == 5050, "Test ca_se fa_iled"
+    assert compute(0) == 0, "Test ca_se fa_iled"
+    assert compute(1) == 1, "Test ca_se fa_iled"
+    assert compute(2) == 3, "Test ca_se fa_iled"
+    assert compute(3) == 6, "Test ca_se fa_iled"
+    assert compute(4) == 10, "Test ca_se fa_iled"
