45940d1f09805befa1f7ff41a4873fd76c7f40b9
amr-mohsen-ahmed.ahmed@alten.com <amr-mohsen-ahmed.ahmed@alten.com>
2025-07-04 20:49:05 +0200

    session1Lab4_soultion: calculate area of a given circle radius

--- session1/lab4_area_circle.py
+++ session1/lab4_area_circle.py
@@ -1,12 +1,15 @@
 """Write a Python program which accepts the radius of a circle and compute the area."""
-
+import math
 
 def area(number1):
     """
     Write a Python program which accepts the radius of a circle from the argument
     and compute the area. Don't forget to return the result at the end.
     """
-
+    number1 = int(input("please enter radius of a circle: "))
+    area_circle = math.pi * (number1**2)
+    print(f"area is: {area_circle}")
+    return area_circle
 
 if __name__ == "__main__":
     assert area(2) == 12.566370614359172, "Test case failed"
