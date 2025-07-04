dd02fc6b26eec9817a017317598b8e5095f2e4ae
amr-mohsen-ahmed.ahmed@alten.com <amr-mohsen-ahmed.ahmed@alten.com>
2025-07-04 21:16:57 +0200

    fixlab1: use lst parameter

--- session1/lab1_list_count.py
+++ session1/lab1_list_count.py
@@ -3,9 +3,9 @@
 
 def count(lst):
     """Write your solution here. Don't forget to return the result at the end...."""
-    given_list  = list(map(int, input("Please give a list with a space: ").split()))
+    lst  = list(map(int, input("Please give a list with a space: ").split()))
     counter = 0
-    for i in given_list :
+    for i in lst:
         if i == 4:
             counter+=1
     print(counter)
