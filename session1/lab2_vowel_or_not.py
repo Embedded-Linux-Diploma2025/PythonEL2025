be5eb9ba978e38ea016575f0bffb9efe914cbfa6
Ahmad Abdullatif <AhmedAbdellatef3846@gmail.com>
2025-07-04 16:54:33 +0300

    session2 labs

--- session1/lab2_vowel_or_not.py
+++ session1/lab2_vowel_or_not.py
@@ -3,7 +3,7 @@
 
 def vowel(letter):
     """Write your solution here. Don't forget to return the result at the end."""
-
+    return letter.lower() in 'aeiou'
 
 if __name__ == "__main__":
     assert vowel("a"), "Test case failed"
