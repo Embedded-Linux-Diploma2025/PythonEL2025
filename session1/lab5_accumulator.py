b520ffa031b7c14876941f88a3c01c47997fa9fc
kimo_elnagar <moon2121999@gmail.com>
2025-07-06 00:25:57 +0300

    session1 tasks solution

--- session1/lab5_accumulator.py
+++ session1/lab5_accumulator.py
@@ -3,7 +3,10 @@
 
 def compute(number):
     """write your soulution Here Sum 1 … n — Compute and display the running total."""
-
+    total = 0
+    for i in range(number):
+        total += i
+    return total + number
 
 if __name__ == "__main__":
     assert compute(5) == 15, "Test case failed"
