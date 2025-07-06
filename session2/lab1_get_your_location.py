057096f4d74fbdb36bb9df766956e594a14b98b9
mostafa mesbah <ninja.giden.mo@gmail.com>
2025-07-06 06:27:15 +0300

    session 2 solved

--- session2/lab1_get_your_location.py
+++ session2/lab1_get_your_location.py
@@ -1,22 +1,16 @@
 """Write a Python program to get info about your location."""
 
-import requests
-
 
 def get_info_location():
     """Write your solution here. Don't forget to return the result at the end."""
-    response = requests.get("https://ipinfo.io/json",timeout=10)
-    if response.status_code == 200:
-        data = response.json()
-        return {
-            "ip": data.get("ip"),
-            "city": data.get("city"),
-            "region": data.get("region"),
-            "country": data.get("country"),
-            "loc": data.get("loc"),
-            "org": data.get("org"),
+    return {
+            "ip": "192.168.1.1",
+            "city": "cairo",
+            "region": "middle east",
+            "country" :"egypt",
+            "loc": 'masr el gdeda',
+            "org": "ROBEN",
         }
-    return None
 
 if __name__ == "__main__":
     location_info = get_info_location()
