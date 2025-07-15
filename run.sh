5d913e05ae9f052ee15e863f3e6c41ae8fe510ad
shadykhaled1 <shady.khaled2012@yahoo.com>
2025-07-15 22:04:44 +0300

    Labs of Session 2 Solution By Shady Khaled

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+examine_session() {
+    local session_dir="$1"
+    cd "$session_dir" || exit 1
+    for file in *.py; do
+        pylint --disable=C0301 "$file"
+        if [ $? -ne 0 ]; then
+            echo "[🟥] Error running pylint for $file"
+            exit 1
+        fi
+        python "$file"
+        if [ $? -ne 0 ]; then
+            echo "[🟥] Error running $file"
+            exit 1
+        fi
+        echo "[🟩] $file ran successfully"
+    done
+    cd ..
+}
+
+#----------------- Session 1 -----------------#
+examine_session "session1"
+#----------------- Session 2 -----------------#
+python session2/lab1_get_your_location.py
+if [ $? -ne 0 ]; then
+    echo "[🟥] Session 2 is not Solved yet"
+    exit 1
+else
+    examine_session "session2"
+fi
+
+
